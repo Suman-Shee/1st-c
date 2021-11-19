46cb66c547e005c908b898951ff93aa3d4f6a586
Suman-Shee <94712791+Suman-Shee@users.noreply.github.com>
2021-11-20 01:04:00 +0530

    1st commit
    1st program

---
+++ 1st.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    int num1, num2, sum ;
+
+
+    printf("enter no  " );
+    scanf("%d", &num1);
+    printf(" enter no ");
+    scanf("%d", &num2);
+    sum = num1 + num2;
+    printf("result is %d", sum);
+    return 0;
+}
